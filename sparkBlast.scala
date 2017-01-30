add7a50a32a35876fc1e26bc6828be662644dccb
marcelorcastro <marcelorcastro@users.noreply.github.com>
2017-01-30 09:03:53 -0300

    Update sparkBlast.scala

--- sparkBlast.scala
+++ sparkBlast.scala
@@ -1,4 +1,4 @@
-/* SimpleApp.scala */
+/* SimpleApp.scala code to execute Blast on cluster of Spark*/
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
@@ -7,16 +7,24 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 object sparkBlast {
   def main(args: Array[String]) {
-    val conf2 = new SparkConf().setAppName("Spark Blast")
+  val conf2 = new SparkConf().setAppName("Spark Blast")
     val sc = new SparkContext(conf2)
     val nos = args(0)
     val conf = new Configuration
+    
+    /* Set delimiter to split file in correct local*/
     conf.set("textinputformat.record.delimiter", ">")
-    conf.set("mapreduce.input.fileinputformat.split.maxsize", nos.toString)
     val script = args(1)
-    val dataset = sc.newAPIHadoopFile(args(2), classOf[TextInputFormat], classOf[LongWritable], classOf[Text], 
-                conf).map(x => x._2.toString).map(x=>x.replace("gi|",">gi|")).pipe(script).
-                saveAsTextFile(args(3))
+    val dataset = sc.newAPIHadoopFile(args(2), classOf[TextInputFormat], classOf[LongWritable], classOf[Text],conf)
+    val mapDataset =  dataset.map(x => x._2.toString)
+    
+    /* Replace first character to add simbol '>'*/
+    val parte2 = mapDataset.map(x=>x.replaceFirst("gi|",">gi|"))
+    val parte3 = parte2.map(x=>x.replace("sp|",">sp|"))
+    val parte4 = parte3.map(x=>x.replace("tr|",">tr|"))
+    
+    /* Option to repartition file in splits defined on 'val nos'*/
+    val repartitionDataset = parte4.repartition(nos.toInt)
+    repartitionDataset.pipe(script).saveAsTextFile(args(3))
     sc.stop
-  }
 }
